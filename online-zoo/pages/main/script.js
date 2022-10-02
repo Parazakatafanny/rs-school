dcba6c725078f399e20d2bcc8766dd7f54242102
Tanya Romanova <heavens1xdoor@mail.ru>
2022-10-02 13:17:51 +0300

    feat: add script burger header

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,5 +1,7 @@
 let emailInput = document.querySelector('.subscribe__input')
 let subscribeButton = document.querySelector('.subscribe__btn')
+let headerBurger = document.querySelector('.header__burger')
+let navWraper = document.querySelector('.nav-wrapper')
 
 emailInput.addEventListener('input', () => {
     if (emailInput.value == '') {
@@ -15,3 +17,8 @@ emailInput.addEventListener('input', () => {
         subscribeButton.classList.add('valid')
     }
 });
+
+headerBurger.addEventListener ('click', () => {
+    headerBurger.classList.toggle('active')
+    navWraper.classList.toggle('active')
+})
