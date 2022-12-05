01abc32e16351ec30bf0a37e838926ba575fc3a6
Tanya Romanova <heavens1xdoor@mail.ru>
2022-12-05 15:55:07 +0300

    feat: configure webpack
    to work with eslint and typescript

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'a5d3372c283f4b278a6251263e1d8223', // получите свой ключ https://newsapi.org/
         });
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
+
+const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
+    devtool: 'inline-source-map',
     mode: 'development',
     module: {
         rules: [
@@ -12,16 +15,22 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+              },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, '../dist'),
     },
     plugins: [
+        new ESLintPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
