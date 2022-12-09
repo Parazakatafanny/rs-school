d5f99dd814506b2c1af20ca8eac479e062910ee0
Tanya Romanova <heavens1xdoor@mail.ru>
2022-12-09 19:34:33 +0300

    feat: migrate appView.js to ts

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,18 +1,24 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { ResponseSource, ResponseEverything } from '../../interfaces/api/main'
+
 
 export class AppView {
+
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    public drawNews(data: ResponseEverything) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    public drawSources(data: ResponseSource) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
