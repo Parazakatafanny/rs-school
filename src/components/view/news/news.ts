899c87a8b754fe3f8ce92049837b062cd74803cc
Tanya Romanova <heavens1xdoor@mail.ru>
2022-12-10 18:06:46 +0300

    refactor: change type style

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -6,7 +6,7 @@ class News {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
         const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
+        const newsItemTemp: HTMLTemplateElement = document.querySelector('#newsItemTemp')!;
 
         news.forEach((item, idx) => {
             const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
