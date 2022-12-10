9137cd37988646bae78ada78926d4104ba96d6d8
Tanya Romanova <heavens1xdoor@mail.ru>
2022-12-10 19:29:02 +0300

    refactor: add proxy

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: 'a5d3372c283f4b278a6251263e1d8223', // получите свой ключ https://newsapi.org/
         });
     }
