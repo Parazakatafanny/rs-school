9ec85f8c6b84604dcc34677fe5ecfcfd64935d9b
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-01-18 07:00:21 +0300

    feat: add start/end animation

--- src/components/garage/car.ts
+++ src/components/garage/car.ts
@@ -1,6 +1,13 @@
 import CartSVG from '@/assets/car.svg';
 import { Car } from '../../interfaces/api';
 
+type EngineData = { velocity: number; distance: number };
+
+enum AnimationOptions {
+  stoped = 'stoped',
+  reset = 'reset',
+}
+
 export default class CarTrack {
   private startButton?: HTMLDivElement;
 
@@ -8,22 +15,79 @@ export default class CarTrack {
 
   private trackMark?: HTMLDivElement;
 
+  private carElem?: SVGSVGElement;
+
+  private isDriving = false;
+
+  private isStopped = false;
+
   constructor(private carId: number) {}
 
-  public startEngine(): Promise<{ velocity: number; distance: number }> {
+  public startEngine(): Promise<EngineData> {
     return new Promise((resolve, reject) => {
-      fetch(`http://127.0.0.1:3000/engine?id=${this.carId}&status=started`)
+      fetch(`http://127.0.0.1:3000/engine?id=${this.carId}&status=started`, {
+        method: 'PATCH',
+      })
         .then((response) => response.json())
-        .then((data) => resolve(data))
+        .then((data) => {
+          resolve(data);
+        })
         .catch(() => reject());
     });
   }
 
-  public startDriving() {
-    
+  public startDriving(engineData: EngineData) {
+    return new Promise((resolve, reject) => {
+      if (!this.carElem) throw new Error();
+      if (!this.trackMark) throw new Error();
+      this.isStopped = true;
+
+      const speed = engineData.distance / engineData.velocity / 1000;
+
+      this.carElem.style.transition = `padding-left ${speed}s linear`;
+      this.carElem.style.paddingLeft = 'calc(100% - 105px)';
+
+      fetch(`http://127.0.0.1:3000/engine?id=${this.carId}&status=drive`, {
+        method: 'PATCH',
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch(() => {
+          this.stopAnimation(AnimationOptions.stoped);
+          reject();
+        });
+    });
   }
 
-  public stopEngine() {}
+  private stopAnimation(options: AnimationOptions) {
+    if (!this.carElem) throw new Error();
+
+    let paddingLeft = '0';
+
+    if (options === AnimationOptions.stoped) {
+      const carStyles = window.getComputedStyle(this.carElem);
+      paddingLeft = carStyles.getPropertyValue('padding-left');
+    } else {
+      this.carElem.style.transition = `padding-left ${0}s linear`;
+    }
+
+    this.carElem.style.paddingLeft = paddingLeft;
+  }
+
+  public stopEngine() {
+    this.isDriving = false;
+    console.log(this.isStopped);
+    return new Promise((resolve, reject) => {
+      fetch(`http://127.0.0.1:3000/engine?id=${this.carId}&status=stopped`, {
+        method: 'PATCH',
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          resolve(data);
+        })
+        .catch(() => reject());
+    });
+  }
 
   public renderCarTrack(parentCar: HTMLElement, carElem: Car) {
     const racePanel = document.createElement('div');
@@ -45,17 +109,28 @@ export default class CarTrack {
     track.innerHTML = CartSVG;
     racePanel.appendChild(track);
 
-    const car = track.querySelector('svg path')!;
-    car.setAttribute('fill', carElem.color);
+    this.carElem = track.querySelector('svg')!;
+    this.carElem.setAttribute('fill', carElem.color);
 
     this.trackMark = document.createElement('div');
     this.trackMark.classList.add('garage__item-finish');
     track.appendChild(this.trackMark);
+    this.attachEvents();
   }
 
   private attachEvents() {
     this.startButton?.addEventListener('click', () => {
-      this.startEngine().then(() => this.startDriving());
+      if (!this.isDriving) {
+        this.isDriving = true;
+        this.startEngine().then((data) => this.startDriving(data));
+      }
+    });
+
+    this.stopButton?.addEventListener('click', () => {
+      if (this.isDriving && this.isStopped) {
+        this.stopEngine();
+        this.stopAnimation(AnimationOptions.reset);
+      }
     });
   }
 }
